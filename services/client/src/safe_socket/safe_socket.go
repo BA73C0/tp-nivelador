9a7c338d8a8d24b30fbaff6709d50cff76016dbb
joaquin Batemarco <joacobatemarco@gmail.com>
2026-08-24 22:20:51 -0300

    feat(WIP): ej4

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -1,21 +1,70 @@
 package safe_socket
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
-//TODO: Complete with a short-read/short-write tolerant implementation
+const MAX_MESSAGE_SIZE = 65535
 
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
+	if len(bytes) > MAX_MESSAGE_SIZE {
+		return errors.New("message too long")
+	}
+
+	var size uint16 = uint16(len(bytes) + 1)
+
+	msg := []byte{byte(size >> 8), byte(size)}
+	msgBytes := make([]byte, size)
+	copy(msgBytes, bytes)
+	msgBytes[size-1] = '\x00'
+	msg = append(msg, msgBytes...)
+
+	err := send(socket, msg, size+uint16(2))
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func send(socket io.Writer, bytes []byte, size uint16) error {
+	n, err := socket.Write(bytes)
+	if err != nil {
+		return err
+	}
+	if n != int(size) {
+		return io.ErrShortWrite
 	}
 	return nil
 }
 
-func RecvAll(socket io.Reader, size int) ([]byte, error) {
-	buff := make([]byte, size)
-	_, err := socket.Read(buff)
+func RecvAll(socket io.Reader, _ int) ([]byte, error) {
+	size, err := recv(socket, 2)
 	if err != nil {
+		return nil, err
+	}
+
+	msgLen := (uint16(size[0]) << 8) | uint16(size[1])
+
+	msg, err := recv(socket, int(msgLen))
+	if err != nil {
+		return nil, err
+	}
+	if len(msg) == 0 || msg[len(msg)-1] != '\x00' {
+		return nil, io.ErrUnexpectedEOF
+	}
+
+	return msg[:len(msg)-1], nil
+}
+
+func recv(socket io.Reader, size int) ([]byte, error) {
+	buff := make([]byte, size)
+	n, err := socket.Read(buff)
+	if n < size {
+		return nil, errors.New("short read")
+	}
+	if err != nil && err != io.EOF {
 		return nil, err
 	}
 	return buff, nil
